a815ee929f9fc92e3ff8690addbae760a1f58b40
Gerard Compion <gcompion93@gmail.com>
2021-10-29 16:44:12 -0400

    Added more conditionals and included logical operators.

--- conditionals.js
+++ conditionals.js
@@ -1,19 +1,25 @@
 const raining = false;
-const temperature = 12;
+const temperature = 41;
 
 if (raining) {
   console.log("Don't forget yoour umbrella!");
-} else {
-  console.log("What's with the umbrella?")
+} else if (!raining) {
+  console.log("You can leave your umbrella at home");
 }
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
-  console.log("Some layers might be a good idea.")
-}
-  else {
-  console.log("Whatever you're wearing is probably fine.")
+  console.log("Some layers might be a good idea.");
+} else if (temperature < -40 || temperature > 40) {
+  console.log("Nobody should be going outside right now...");
+} else {
+  console.log("Whatever you're wearing is probably fine.");
 }
 
-console.log("Now you're ready to go outside!")
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age >= 18) {
+  console.log("You are eligible to vote.")
+}

4a31cd7b43aaf27dee70cec9acc117742f9c1c43
Gerard Compion <gcompion93@gmail.com>
2021-10-29 23:54:16 -0400

    Fixed a typo

--- chorus-while.js
+++ chorus-while.js
@@ -2,7 +2,7 @@ const chorus = "Let's dance!";
 let repeat = 0;
 while (repeat < 10) {
   if (repeat === 5) {
-    console.log("*change key*")
+    console.log("*change key*");
   }
   console.log(chorus);
   repeat++;
